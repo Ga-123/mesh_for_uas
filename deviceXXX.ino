f53178961720cb3e57fddbe0acb155313238b30e
Ga-123 <55543145+Ga-123@users.noreply.github.com>
2024-11-17 17:49:08 +0300

    Update deviceXXX.ino

--- deviceXXX.ino
+++ deviceXXX.ino
@@ -1,7 +1,9 @@
 #include "meshWrap.h"
 
 const int ledPin = 2;      // Пин для светодиода
-const uint8_t nodeID = 2;  // Уникальный ID релейного узла (для каждого устройства меняем ID)
+const uint8_t nodeID = 2;  // Уникальный ID узла (для каждого устройства меняем ID)
+
+static unsigned long ctr = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -10,6 +12,20 @@ void setup() {
 
 void loop() {
   updateMesh();             // Обновление состояния сети
-  sendScheduledMessage();   // Отправка сообщений
-  receiveMessages();        // Обработка входящих сообщений
+
+  payload_t data_to_send;
+  data_to_send.ms = millis();
+  data_to_send.counter = ctr;
+
+  sendScheduledMessage(data_to_send);   // Отправка сообщений
+
+  RF24NetworkHeader header;
+  payload_t payload;
+  if (receiveMessages(header, payload)) {
+    // Вывод информации о полученных данных
+    Serial.print("Received packet #");
+    Serial.print(payload.counter);
+    Serial.print(" at ");
+    Serial.println(payload.ms);
+  }
 }
